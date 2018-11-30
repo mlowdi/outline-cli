e809841acf8c74aef1e4ce4088e5032cce1afa5e
Martin L. Fällman <martin.fallman@crd.org>
2018-11-30 10:58:46 +0100

    added validation of server fingerprint against server definition file

--- outline-cli.py
+++ outline-cli.py
@@ -3,15 +3,17 @@
 import argparse
 import json
 import requests
+import ssl
+from requests_toolbelt.adapters.fingerprint import FingerprintAdapter
 import urllib
 
 ACCESS_PREAMBLE = "https://s3.amazonaws.com/outline-vpn/invite.html#"
 servers = {}
 server_names = []
 
-# We know what we're doing, we don't need warnings
 # Outline servers use self-signed certs
-# TODO: figure out how to verify the cert agains the sha256 sum in the server definition
+# We're using requests_toolbelt to compare the certificate sha256 against the definition
+# and will throw an error if they don't match
 requests.packages.urllib3.disable_warnings()
 
 
@@ -37,11 +39,17 @@ def call_api(action, server, userid=False):
 	if action == 'deluser' and not userid:
 		raise Exception('Cannot call deluser without passing a User ID')
 
+	# Construct the URL string to use and get the sha256 fp from the server definition
 	urlline = [servers[server]['apiUrl'], '/access-keys']
+	certfp = servers[server]['certSha256']
+
+	# Mount adapter to verify fingerprint
+	s = requests.Session()
+	s.mount(urlline[0], FingerprintAdapter(certfp))
 
 	if action == "adduser":
 		try:
-			r = requests.post(''.join(urlline), verify=False)
+			r = s.post(''.join(urlline), verify=False)
 			pass
 		except Exception as e:
 			raise e
@@ -49,7 +57,7 @@ def call_api(action, server, userid=False):
 	elif action == "deluser":
 		urlline.append("/" + str(userid))
 		try:
-			r = requests.delete(''.join(urlline), verify=False)
+			r = s.delete(''.join(urlline), verify=False)
 			pass
 		except Exception as e:
 			raise e
@@ -62,7 +70,7 @@ def call_api(action, server, userid=False):
 	
 	elif action == "listusers":
 		try:
-			r = requests.get(''.join(urlline), verify=False)
+			r = s.get(''.join(urlline), verify=False)
 			pass
 		except Exception as e:
 			raise e
@@ -103,28 +111,31 @@ def prettyrecord(accesskey):
 		print s.format(accesskey['id'], accesskey['name'], accesskey['accessUrl'])
 
 
-# Argparse setup (takes the server list, so we invoke it first)
-load_servers()
-parser = argparse.ArgumentParser(description='Fleet management for Outline VPN servers')
-parser.add_argument('-s', '--server', required=True, help='Server to take action on', choices=server_names)
-actions = parser.add_mutually_exclusive_group(required=True)
-actions.add_argument('-a', '--adduser', action='store_true', help="Create a user key on server")
-actions.add_argument('-d', '--deluser', metavar='ID', type=int, help="Delete user key with given ID from server")
-actions.add_argument('-l', '--list', action='store_true', help="List all user keys on server")
-parser.add_argument('-i', action='store_true', help="Add one-click invitation links to output")
-arguments = parser.parse_args()
+if __name__ == '__main__':
+	# Argparse setup (takes the server list, so we invoke it first)
+	load_servers()
+	parser = argparse.ArgumentParser(description='Fleet management for Outline VPN servers')
+	parser.add_argument('-s', '--server', required=True, help='Server to take action on', choices=server_names)
+	actions = parser.add_mutually_exclusive_group(required=True)
+	actions.add_argument('-a', '--adduser', action='store_true', help="Create a user key on server")
+	actions.add_argument('-d', '--deluser', metavar='ID', type=int, help="Delete user key with given ID from server")
+	actions.add_argument('-l', '--list', action='store_true', help="List all user keys on server")
+	parser.add_argument('-n', metavar='name', help="Set a friendly name for the newly created user")
+	parser.add_argument('-i', action='store_true', help="Add one-click invitation links to output")
+
+	arguments = parser.parse_args()
 
 
-# Invoke correct function
-if arguments.adduser:
-	print "Creating a user on server %s" % (arguments.server)
-	adduser(arguments.server)
+	# Invoke correct function
+	if arguments.adduser:
+		print "Creating a user on server %s" % (arguments.server)
+		adduser(arguments.server)
 
-if arguments.deluser:
-	print "Deleting user key number %s from server %s" % (arguments.deluser, arguments.server)
-	deluser(arguments.server, arguments.deluser)
+	if arguments.deluser:
+		print "Deleting user key number %s from server %s" % (arguments.deluser, arguments.server)
+		deluser(arguments.server, arguments.deluser)
 
-if arguments.list:
-	print "Listing user keys on server %s" % arguments.server
-	listusers(arguments.server)
+	if arguments.list:
+		print "Listing user keys on server %s" % arguments.server
+		listusers(arguments.server)
 
